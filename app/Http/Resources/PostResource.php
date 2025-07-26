8e7387fcfa023b937a74470ea799611475c793cd
rahmandayub <rahmandayub@gmail.com>
2025-07-26 13:25:50 +0700

    Add bio and profile_photo to user in PostResource

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -15,31 +15,37 @@ class PostResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'title' => $this->title,
-            'slug' => $this->slug,
-            'content' => $this->content,
-            'featured_image' => $this->featured_image
-                ? (preg_match('/^https?:\/\//', $this->featured_image)
+            "id" => $this->id,
+            "title" => $this->title,
+            "slug" => $this->slug,
+            "content" => $this->content,
+            "featured_image" => $this->featured_image
+                ? (preg_match("/^https?:\/\//", $this->featured_image)
                     ? $this->featured_image
-                    : 'uploads/' . ltrim($this->featured_image, '/'))
+                    : "uploads/" . ltrim($this->featured_image, "/"))
                 : null,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-            'category' => $this->category ? [
-                'id' => $this->category->id,
-                'name' => $this->category->name,
-            ] : null,
-            'tags' => $this->tags->map(function ($tag) {
+            "created_at" => $this->created_at,
+            "updated_at" => $this->updated_at,
+            "category" => $this->category
+                ? [
+                    "id" => $this->category->id,
+                    "name" => $this->category->name,
+                ]
+                : null,
+            "tags" => $this->tags->map(function ($tag) {
                 return [
-                    'id' => $tag->id,
-                    'name' => $tag->name,
+                    "id" => $tag->id,
+                    "name" => $tag->name,
                 ];
             }),
-            'user' => $this->user ? [
-                'id' => $this->user->id,
-                'name' => $this->user->name,
-            ] : null,
+            "user" => $this->user
+                ? [
+                    "id" => $this->user->id,
+                    "name" => $this->user->name,
+                    "bio" => $this->user->bio,
+                    "profile_photo" => $this->user->profile_photo,
+                ]
+                : null,
         ];
     }
 }
